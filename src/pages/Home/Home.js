9fd8137f4089a2a274573ab38598352900152e7a
duanxiaojie <905024728@qq.com>
2019-05-16 11:22:22 +0800

    update516

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     const user = window.sessionStorage.getItem('userInfo') ? JSON.parse(window.sessionStorage.getItem('userInfo')) : {};
     return (
       <div>
-        登录成功！欢迎<span style={{color: '#3b99f0', padding: '0 3px'}}>{user.username}</span>，新的一天从此刻开始！
+        登录成功！欢迎<span style={{color: '#3b99f0', padding: '0 3px'}}>{user.username}</span>回来，祝您生活愉快！
       </div>
     )
   }
